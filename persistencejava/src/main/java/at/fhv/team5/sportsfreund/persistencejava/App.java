40faa77ac8dbe52f949f2d3f8dea018de0535796
Bianca <bianca.koinegg@hotmail.com>
2019-03-27 19:34:05 +0100

    init Project

---
+++ persistencejava/src/main/java/at/fhv/team5/sportsfreund/persistencejava/App.java
@@ -0,0 +1,13 @@
+package at.fhv.team5.sportsfreund.persistencejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
